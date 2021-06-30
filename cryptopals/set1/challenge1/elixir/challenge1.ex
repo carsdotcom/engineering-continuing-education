3af4487440eae5393cddd9cbeaa7f55361e3b579
jersearls <searls@me.com>
2021-06-30 16:58:50 -0400

    follow single file convention

--- cryptopals/set1/challenge1/elixir/challenge1.ex
+++ cryptopals/set1/challenge1/elixir/challenge1.ex
@@ -13,3 +13,22 @@ defmodule CryptoPals.Set1.Challenge1 do
     String.to_integer(string)
   end
 end
+
+ExUnit.start()
+ExUnit.configure(exclude: :pending, trace: true)
+
+defmodule CryptoPals.Set1.Challenge1Test do
+  @moduledoc """
+  Tests for Cryptopals Challenge 1.
+  For a submission to be considered "complete", all tests should pass.
+  """
+
+  use ExUnit.Case
+  alias CryptoPals.Set1.Challenge1
+
+  describe "example_fn/1" do
+    test "given a string, produces an integer" do
+      assert Challenge1.example_fn("1") == 1
+    end
+  end
+end

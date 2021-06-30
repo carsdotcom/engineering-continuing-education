ceb23713da614542635c7bc353f8b2407746fecd
jersearls <searls@me.com>
2021-06-30 12:38:15 -0400

    create skeleton for first week of challenges

---
+++ cryptopals/set1/challenge1/elixir/challenge1.ex
@@ -0,0 +1,6 @@
+defmodule CryptoPals.Set1.Challenge1 do
+  @moduledoc """
+  Solution code should be written here. Tests can be found at
+  CryptoPals.Set1.Challenge1Test.
+  """
+end
---
+++ cryptopals/set1/challenge1/elixir/challenge1test.exs
@@ -0,0 +1,10 @@
+defmodule CryptoPals.Set1.Challenge1Test do
+  @moduledoc """
+  Tests for Cryptopals Challenge 1.
+  For a submission to be considered "complete", all tests should pass.
+  """
+
+  use ExUnit.Case
+
+  alias CryptoPals.Set1.Challenge1
+end

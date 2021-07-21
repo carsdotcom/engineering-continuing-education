4671dea1eaa041eb3100642aaa3ba18302469738
jersearls <searls@me.com>
2021-07-21 15:14:26 -0400

    add new template

---
+++ cryptopals/set1/challenge2/elixir_template/challenge2_template.ex
@@ -0,0 +1,42 @@
+defmodule CryptoPals.Set1.Challenge2 do
+  @moduledoc """
+  Source: 'https://cryptopals.com/sets/1/challenges/2'
+
+  Solution code should be written here.
+
+  To run tests, paste the following into the commandline from the project root.
+  ```bash
+  elixir -r cryptopals/set1/challenge2/elixir_template/challenge2_template.ex
+  ```
+
+  When submitting, change the name of this file to reflect the names of those that participated in the solution. (ex: `challenge1_mary_linda_harriet.ex`)
+  Then create a PR to add your file to the repo under `cryptopals/set1/challenge1/submitted_solutions/`.
+  """
+
+  @doc """
+  This is an example fn to serve the example test file. Please delete it and write your own ;)
+  """
+  @spec example_fn(String.t()) :: integer()
+  def example_fn(string) do
+    String.to_integer(string)
+  end
+end
+
+ExUnit.start()
+ExUnit.configure(exclude: :pending, trace: true)
+
+defmodule CryptoPals.Set1.Challenge2Test do
+  @moduledoc """
+  Tests for Cryptopals Challenge 1.
+  For a submission to be considered "complete", all tests should pass.
+  """
+
+  use ExUnit.Case
+  alias CryptoPals.Set1.Challenge1
+
+  describe "example_fn/1" do
+    test "given a string, produces an integer" do
+      assert Challenge1.example_fn("1") == 1
+    end
+  end
+end

9e602822888f45916733bf47178e282c687ac642
jersearls <searls@me.com>
2021-08-18 16:38:20 -0400

    first pass at challenge 4

--- cryptopals/set1/challenge4/elixir_template/challenge4_template.ex
+++ cryptopals/set1/challenge4/elixir_template/challenge4_template.ex
@@ -1,4 +1,6 @@
 defmodule CryptoPals.Set1.Challenge4 do
+  use Bitwise
+
   @moduledoc """
   Source: 'https://cryptopals.com/sets/1/challenges/4'
 
@@ -24,9 +26,85 @@ defmodule CryptoPals.Set1.Challenge4 do
   @doc """
   This is an example fn to serve the example test file. Please delete it and write your own ;)
   """
-  @spec example_fn(String.t()) :: integer()
-  def example_fn(string) do
-    String.to_integer(string)
+
+  # Taken from this article https://www3.nd.edu/~busiforc/handouts/cryptography/letterfrequencies.html
+
+  @file_path Path.expand(__DIR__) <> "/challenge4.txt"
+
+  @spec xorcist() :: String.t()
+  def xorcist do
+    lines()
+    |> Enum.map(&decode_hex_to_binary/1)
+    |> Enum.flat_map(&evaluate/1)
+    |> score_spaces()
+    |> Enum.sort_by(fn {_list, score} -> score end)
+    |> List.take(10)
+    |> Enum.map(&elem(&1, 0))
+    |> List.to_string()
+  end
+
+  defp evaluate(decoded_byte_list) do
+    Enum.map(make_characters(), fn character ->
+      Enum.map(decoded_byte_list, fn integer ->
+        bxor(character, integer)
+      end)
+    end)
+  end
+
+  @spec lines() :: [[integer()]]
+  defp lines do
+    @file_path
+    |> File.stream!([:line])
+    |> Stream.map(&String.trim_trailing/1)
+    |> Enum.to_list()
+  end
+
+  @make_characters MapSet.new(0..255)
+  defp make_characters do
+    # make each ascii character (each being a byte)
+    @make_characters
+  end
+
+  @printable_characters MapSet.new(32..126)
+  defp filter_printable(charlists) do
+    Enum.filter(charlists, fn list ->
+      Enum.all?(list, fn char ->
+        char in @printable_characters
+      end)
+    end)
+  end
+
+  @spec decode_hex_to_binary(String.t()) :: [integer()]
+  defp decode_hex_to_binary(string) do
+    string
+    |> Base.decode16!(case: :lower)
+    |> :binary.bin_to_list()
+  end
+
+  @letters_sorted_by_frequency ~w(e a r i o t n s l c u d p m h g b f y w k v x z j q)
+  @range 26..1
+  @letter_scores @letters_sorted_by_frequency |> Enum.zip(@range) |> Map.new()
+
+  @spec score(String.t()) :: {String.t(), non_neg_integer()}
+  defp score(string) do
+    {string,
+     string
+     |> String.graphemes()
+     |> Enum.reduce(0, fn grapheme, running_total ->
+       running_total + Map.get(@letter_scores, grapheme, 0)
+     end)}
+  end
+
+  @average_word_length 4.7
+  @spec score_spaces(List.t(List.t())) :: [{List.t(), integer()}]
+  defp score_spaces(lists) do
+    Enum.map(lists, fn list ->
+      space_target = length(list) / (@average_word_length + 1)
+      count = Enum.count(list, fn char -> char == 32 end)
+      score = abs(count - space_target)
+
+      {list, score}
+    end)
   end
 end
 
@@ -42,9 +120,10 @@ defmodule CryptoPals.Set1.Challenge4Test do
   use ExUnit.Case
   alias CryptoPals.Set1.Challenge4
 
-  describe "example_fn/1" do
-    test "given a string, produces an integer" do
-      assert Challenge4.example_fn("1") == 1
+  describe "xorcist/1" do
+    test "given a file, produces list of strings" do
+      Challenge4.xorcist()
+      |> IO.inspect()
     end
   end
 end

32531852dd584e208c7a9e98c885eaa94988aa8c
Christian Koch <ckoch@cars.com>
2021-08-19 21:44:20 -0500

    move files and solutions through challenge 6

--- cryptopals/set1/challenge1/submitted_solutions/ck_pw_p1.ex
+++ cryptopals/set1/challenge1/submitted_solutions/ck_pw_p1.ex
@@ -14,13 +14,8 @@ defmodule Decode do
 
     :binary.bin_to_list(i1)
     |> bxor_lists(:binary.bin_to_list(i2))
-    # |> Enum.zip_reduce(:binary.bin_to_list(i2), [], fn one, two, acc ->
-    #   [bxor(one, two) | acc]
-    # end)
-    # |> IO.inspect(label: "two zipped")
     |> Enum.reverse()
     |> to_string()
-    # |> IO.inspect(label: "two reversed zipped")
     |> encode16(case: :lower)
   end
 
@@ -30,53 +25,12 @@ defmodule Decode do
     end)
   end
 
-  def three1(input1) do
-    i1 = decode16!(input1, case: :mixed)
-
-    i1bin = :binary.bin_to_list(i1)
-    base_chars = 0..255
-
-    Enum.map(base_chars, fn char ->
-      Enum.map(i1bin, fn input_char ->
-        bxor(input_char, char)
-      end)
-      # |> List.reverse()
-      |> to_string()
-      # |> IO.inspect(label: "sorted three")
-      |> score(char)
-    end)
-    |> sort()
-    |> List.first()
-  end
-
-  def three2(input1) do
-    i1 = decode16!(input1, case: :mixed)
-
-    i1bin = :binary.bin_to_list(i1)
-    base_chars = 0..512
-
-    Enum.map(base_chars, fn char ->
-      i1bin
-      |> Enum.reduce([], fn one, acc ->
-        [bxor(one, char) | acc]
-      end)
-      |> Enum.reverse()
-      |> to_string()
-      |> score(char)
-    end)
-    |> sort()
-    |> List.first()
-  end
-
-  def three3(input1, comparison_chars) do
+  def three(input1, comparison_chars) do
     i1 = decode16!(input1, case: :mixed)
 
     i1bin = :binary.bin_to_list(i1)
     len = length(i1bin)
     Enum.map(comparison_chars, fn char ->
-      # buffer_for = for _i <- 1..(len) do
-      #   char
-      # end
       buffer_dupe = List.duplicate(char, len)
 
       bxor_lists(i1bin, buffer_dupe)
@@ -88,42 +42,72 @@ defmodule Decode do
     |> List.first()
   end
 
-
   def four(data) do
     data
-    |> Enum.map(fn input -> three2(input) end)
+    |> Enum.map(fn input -> three(input, 0..255) end)
     |> sort()
     |> List.first()
   end
 
-  def five(input, key) do
+  def five_encode(input, key) do
     i1bin = to_charlist(input)
-
     len = length(i1bin)
-    ilen = String.length(input)
-
-    (len == ilen) |> IO.inspect(label: "lengths?")
-
-    index_bin = Enum.with_index(i1bin)
-    # key_len = String.length(key)
+    key_len = String.length(key)
     chars = to_charlist(key)
 
     buffer_for = for i <- 0..(len - 1) do
-      Enum.at(chars, Integer.mod(i, 3))
+      Enum.at(chars, Integer.mod(i, key_len))
     end
-    |> IO.inspect(label: "buffer_for")
 
-    xorred = bxor_lists(i1bin, buffer_for)
-
-    x_str = xorred
+    i1bin
+    |> bxor_lists(buffer_for)
     |> Enum.reverse()
     |> to_string()
-
-    m_str = Enum.map(index_bin, fn {char, idx}  ->
-      bxor(char, Enum.at(chars, Integer.mod(idx, 3)))
-    end)
-    |> to_string()
     |> encode16(case: :lower)
+  end
+
+  def five_decode(input, key) do
+    i1bin = decode16!(input, case: :mixed)
+    i1binlist = :binary.bin_to_list(i1bin)
+    len = byte_size(i1bin)
+    key_len = String.length(key)
+    chars = to_charlist(key)
+
+    buffer_for = for i <- 0..(len - 1) do
+      Enum.at(chars, Integer.mod(i, key_len))
+    end
+
+    i1binlist
+    |> bxor_lists(buffer_for)
+    |> Enum.reverse()
+    |> to_string()
+  end
+
+  def six(input) do
+    maybe_keys = Decode.keysize(input)
+    Enum.map(maybe_keys, fn {_, keysize} ->
+      input
+      |> Decode.transpose(keysize)
+      |> Enum.map(fn tuple ->
+        detect_xor_byte(tuple)
+      end)
+    end)
+  end
+
+  def detect_xor_byte(input) do
+    base_chars = 0..255
+
+    len = tuple_size(input)
+    Enum.map(base_chars, fn char ->
+      buffer_dupe = List.duplicate(char, len)
+      list = Tuple.to_list(input)
+      bxor_lists(list, buffer_dupe)
+      |> Enum.reverse()
+      |> to_string()
+      |> score(char)
+    end)
+    |> sort()
+    |> List.first()
   end
 
   def distance(string1, string2) do
@@ -134,117 +118,94 @@ defmodule Decode do
       xor = bxor(one, two)
       xor_digits = Integer.digits(xor, 2)
       xor_sum = Enum.sum(xor_digits)
-      |> IO.inspect(label: "sum")
       acc + xor_sum
     end)
   end
 
+  def keysize(input) do
+    6..48
+    |> Enum.map(fn i ->
+      buf1 = String.slice(input, 0, i)
+      buf2 = String.slice(input, i, i)
+      buf3 = String.slice(input, (i * 2), i)
+      buf4 = String.slice(input, (i * 3), i)
+      buf5 = String.slice(input, (i * 4), i)
+      buf6 = String.slice(input, (i * 5), i)
 
-    # distance/2 false-starts
-    # length(bs1)  |> IO.inspect(label: "")
-    # length(bs2)  |> IO.inspect(label: "")
+      dist1 = distance(buf1, buf2)
+      dist2 = distance(buf2, buf3)
+      dist3 = distance(buf3, buf4)
+      dist4 = distance(buf4, buf5)
+      dist5 = distance(buf5, buf6)
 
-    # l1 = Enum.flat_map(bs1, fn one ->
-    #   Integer.digits(one, 2)
-    #   |> IO.inspect(label: "")
-    # end)
-    # |> length
-    # |> IO.inspect(label: "LENGTH1")
-    # # |> IO.inspect(label: "list ONE")
-    # l2 = Enum.flat_map(bs2, fn two ->
-    #   Integer.digits(two, 2)
-    #   |> IO.inspect(label: "")
-    # end)
-    # |> length
-    # |> IO.inspect(label: "LENGTH2")
-    # |> IO.inspect(label: "list TWO")
+      avg_dist = (dist1 + dist2 + dist3 + dist4 + dist5) / (5 * i)
 
-    # Enum.zip_reduce(l1, l2, 0, fn one, two, acc ->
-    #   acc + if one == two do
-    #     0
-    #   else
-    #     1
-    #   end
-    #   # |> IO.inspect(label: "curr accum")
-    # end)
-    # Enum.zip_reduce(bs1, bs2, 0, fn one, two, acc ->
+      {avg_dist, i}
+    end)
+    |> sort(&</2)
+    |> Enum.take(6)
+  end
 
-      # l1 = Integer.digits(one, 2)
-      # |> IO.inspect(label: "")
-      # l2 = Integer.digits(two, 2)
-      # |> IO.inspect(label: "")
-      # len1 = length(l1)
-      # |> IO.inspect(label: "LENGTH")
-      # len2 = length(l2)
-      # |> IO.inspect(label: "LENGTH")
-      # len = if len1 > len2 do
-      #   len1
-      # else
-      #   len2
-      # end
+  def transpose(input_string, keysize) do
+    input_string
+    |> :binary.bin_to_list()
+    |> Enum.chunk_every(keysize)
+    |> pad_last(keysize)
+    |> Enum.zip()
+  end
 
-      # Enum.reduce(0..(len-1), acc, fn i, inner_acc ->
-      #   incr = if Enum.at(l1, i) == Enum.at(l2, i) do
-      #     0
-      #   else
-      #     1
-      #   end
-      #   inner_acc + incr
-      # end)
-      # |> IO.inspect(label: "inner accum")
-    # end)
+  defp pad_last(chunks, keysize) do
+    last = List.last(chunks)
+    list_len = length(last)
 
-    # Enum.reduce(bs1, {bs1, bs2, 0}, fn _, {l1, l2, a1} ->
-    #   [h1 | t1] = l1
-    #   # Integer.digits(h1), 2)
-    #   # h1 |> IO.inspect(label: "h1")
-    #   [h2 | t2] = l2
-    #   # h2 |> IO.inspect(label: "h2")
-    #   # diff = Integer.digits(h1, 2) - Integer.digits(h2, 2)
+    if list_len < keysize do
+      dupe = List.duplicate(0, (keysize - list_len))
+      new_last = last ++ dupe
+      List.replace_at(chunks, -1, new_last)
+    else
+      chunks
+    end
+  end
 
-    #   a1 |> IO.inspect(label: "OUTER accumulator")
-    #   a_diff = Enum.zip_reduce(Integer.digits(h1, 2), Integer.digits(h2, 2), a1, fn one, two, acc ->
-    #     acc + if one == two do
-    #       0
-    #     else
-    #       1
-    #     end
-    #     |> IO.inspect(label: "curr accum")
-
-    #   end)
-    #   # |> IO.inspect(label: "ADIFF")
-    #   {t1, t2, a_diff}
-    # end)
-    # |> IO.inspect(label: "reduction")
-    # Enum.reduce(bs1, fn _ ->
-
-    # end)
-    # (bs1 - bs2) |> IO.inspect(label: "diff")
-    # Enum.zip_reduce(bs1, bs2, 0, fn one, two, acc ->
-    #   bump =  if one == two do
-    #     1
-    #   else
-    #     0
-    #   end
-    #   acc + bump
-    # end)
-    # |> IO.inspect(label: "")
-
-
-  defp sort(list) do
-    Enum.sort(list, fn {one, _, _}, {two, _, _} ->
-      one >= two
+  def sort(list, sorter \\ &>=/2)
+  def sort([{_, _} | _] = list, sorter) do
+    Enum.sort(list, fn {one, _}, {two, _} ->
+      sorter.(one, two)
     end)
   end
 
-  defp score(string, char) do
+  def sort([{_, _, _} | _] = list, sorter) do
+    Enum.sort(list, fn {one, _, _}, {two, _, _} ->
+      sorter.(one, two)
+    end)
+  end
+
+  def sort([{_, _, _, _} | _] = list, sorter) do
+    Enum.sort(list, fn {one, _, _, _}, {two, _, _, _} ->
+      sorter.(one, two)
+    end)
+  end
+
+  defp score(string, char) when is_binary(string) do
     alphabet = "abcdefghijklmnopqrstuvwxyz "
 
     base_64_chars = String.codepoints(alphabet)
 
     pts = String.codepoints(String.downcase(string))
     score = Enum.count(pts, fn pt -> pt in base_64_chars end)
-    {score, <<char::utf8>>, string}
+    # score = Enum.count(pts, fn pt -> pt in base_64_chars end)
+    {score, <<char::utf8>>, char, string}
+  end
+
+  def eval(string, key) when is_binary(string) do
+    alphabet = "abcdefghijklmnopqrstuvwxyz "
+
+    base_64_chars = String.codepoints(alphabet)
+
+    pts = String.codepoints(String.downcase(string))
+    score = Enum.count(pts, fn pt -> pt in base_64_chars end)
+    # score = Enum.count(pts, fn pt -> pt in base_64_chars end)
+    {score, key, string}
   end
 end
 
@@ -266,35 +227,33 @@ defmodule DecodeTest do
            ) == "746865206b696420646f6e277420706c6179"
   end
 
-  test "three/1" do
-    assert Decode.three1("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736") ==
-             {33, "X", "Cooking MC's like a pound of bacon"}
-  end
-
-  test "three-2" do
-    assert Decode.three2("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736") ==
-             {33, "X", "Cooking MC's like a pound of bacon"}
-  end
-
-  test "three-3" do
-    assert Decode.three3("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736", 0..255) ==
-             {33, "X", "Cooking MC's like a pound of bacon"}
+  test "three" do
+    assert Decode.three("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736", 0..255) ==
+             {33, "X", 88, "Cooking MC's like a pound of bacon"}
   end
 
   test "four/1" do
-    {:ok, contents} = File.read("cryptopals/set1/challenge1/data/4.txt")
+    {:ok, contents} = File.read("cryptopals/set1/data/4.txt")
     list = String.split(contents, "\n")
 
-    assert Decode.four(list) == {29, "5", "Now that the party is jumping\n"}
+    assert Decode.four(list) == {29, "5", 53, "Now that the party is jumping\n"}
   end
 
-  test "five/2" do
-    input = "Burning 'em, if you ain't quick and nimble
-I go crazy when I hear a cymbal"
+  test "five_encode/2" do
+    input = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
     # """
     key = "ICE"
 
-    assert Decode.five(input, key) == "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+    assert Decode.five_encode(input, key) == "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+  end
+
+  test "five_decode/2" do
+    input = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+    output = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
+    # """
+    key = "ICE"
+
+    assert Decode.five_decode(input, key) == output
   end
 
   test "distance/2" do
@@ -303,17 +262,67 @@ I go crazy when I hear a cymbal"
     assert Decode.distance(one, two) == 37
   end
 
-  # test "six/2" do
-  #   {:ok, contents} = File.read("cryptopals/set1/challenge1/data/4.txt")
-  #   list = String.split(contents, "\n")
+  test "keysize/1" do
+    {:ok, contents} = File.read("cryptopals/set1/data/6.txt")
+    bin_contents = Base.decode64!(contents, ignore: :whitespace)
+    [{_norm_dist, keysize} | _] = Decode.keysize(bin_contents)
 
-  #   assert Decode.four(list) == {29, "5", "Now that the party is jumping\n"}
+    assert keysize == 29
+  end
 
-#     input = "Burning 'em, if you ain't quick and nimble
-# I go crazy when I hear a cymbal"
-#     # """
-#     key = "ICE"
+  test "transpose short strings into n" do
+    contents  = "HUIfTQsPAh9PE048"
+    keysize = 4
+    transposed = Decode.transpose(contents, keysize)
+    transposed_list = Enum.map(transposed, &(Tuple.to_list(&1)))
+    assert transposed_list == ['HTAE', 'UQh0', 'Is94', 'fPP8']
 
-#     assert Decode.five(input, key) == "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
-  # end
+    keysize = 8
+    transposed = Decode.transpose(contents, keysize)
+    transposed_list = Enum.map(transposed, &(Tuple.to_list(&1)))
+    assert transposed_list == ['HA', 'Uh', 'I9', 'fP', 'TE', 'Q0', 's4', 'P8']
+
+    keysize = 2
+    transposed = Decode.transpose(contents, keysize)
+    transposed_list = Enum.map(transposed, &(Tuple.to_list(&1)))
+    assert transposed_list == ['HITsA9E4', 'UfQPhP08']
+  end
+
+  test "transpose a string into 4" do
+    contents  = "HUIfTQsPAh9PE048GmllH0kcDk4TAQsHThsBFkU2AB4BSWQgVB0dQzNTTmVS"
+    keysize = 4
+    transposed = Decode.transpose(contents, keysize)
+    transposed_list = Enum.map(transposed, &(Tuple.to_list(&1)))
+    assert transposed_list == ['HTAEGHDATFASVQT', 'UQh0m0kQhkBWBzm', 'Is94lk4ssU4Q0NV', 'fPP8lcTHB2BgdTS']
+  end
+
+  test "transpose" do
+    {:ok, contents} = File.read("cryptopals/set1/data/6.txt")
+    keysize = 10
+    transposed = Decode.transpose(contents, keysize)
+    assert length(transposed) == keysize
+    str_len = String.length(contents)
+    first = List.first(transposed) |> Tuple.to_list()
+    assert length(first) == (str_len / keysize) |> trunc()
+  end
+
+  test "six" do
+    {:ok, contents} = File.read("cryptopals/set1/data/6.txt")
+    bin_contents = Base.decode64!(contents, ignore: :whitespace)
+    binary_encoded = bin_contents
+    |> Base.encode16(case: :lower)
+
+    key_candidates = Decode.six(bin_contents)
+    {_, key, decoded} = Enum.map(key_candidates, fn cand ->
+      key = Enum.reduce(cand, "", fn {_, char, _, _}, acc -> acc <> char end)
+      decoded_str = Decode.five_decode(binary_encoded, key)
+      Decode.eval(decoded_str, key)
+    end)
+    |> Decode.sort()
+    |> List.first
+
+    assert key == "Terminator X: Bring the noise"
+    last_n_chars = String.slice(decoded, -23, 23)
+    assert last_n_chars == "Play that funky music \n"
+  end
 end

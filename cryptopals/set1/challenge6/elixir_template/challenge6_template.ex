e1927f5cba1c49b6e5fcb810f3ffd4e51be6d0f1
Christian Koch <ckoch@cars.com>
2021-09-22 13:21:31 -0500

    format

--- cryptopals/set1/challenge6/elixir_template/challenge6_template.ex
+++ cryptopals/set1/challenge6/elixir_template/challenge6_template.ex
@@ -24,28 +24,35 @@ defmodule CryptoPals.Set1.Challenge6 do
 
   Here's how:
 
-  Let KEYSIZE be the guessed length of the key; try values from 2 to (say) 40.
-  Write a function to compute the edit distance/Hamming distance between two strings. The Hamming distance is just the number of differing bits. The distance between:
+    1) Let KEYSIZE be the guessed length of the key; try values from 2 to (say) 40.
 
-  `this is a test`
+    2) Write a function to compute the edit distance/Hamming distance between two strings. The Hamming distance is just the number of differing bits. The distance between:
 
-  and
+      `this is a test`
 
-  `wokka wokka!!!`
+      and
 
-  is 37. Make sure your code agrees before you proceed.
+      `wokka wokka!!!`
 
-  For each KEYSIZE, take the first KEYSIZE worth of bytes, and the second KEYSIZE worth of bytes, and find the edit distance between them. Normalize this result by dividing by KEYSIZE.
-  The KEYSIZE with the smallest normalized edit distance is probably the key. You could proceed perhaps with the smallest 2-3 KEYSIZE values. Or take 4 KEYSIZE blocks instead of 2 and average the distances.
-  Now that you probably know the KEYSIZE: break the ciphertext into blocks of KEYSIZE length.
-  Now transpose the blocks: make a block that is the first byte of every block, and a block that is the second byte of every block, and so on.
-  Solve each block as if it was single-character XOR. You already have code to do this.
-  For each block, the single-byte XOR key that produces the best looking histogram is the repeating-key XOR key byte for that block. Put them together and you have the key.
+      is 37. Make sure your code agrees before you proceed.
+
+    3) For each KEYSIZE, take the first KEYSIZE worth of bytes, and the second KEYSIZE worth of bytes, and find the edit distance between them. Normalize this result by dividing by KEYSIZE.
+
+    4) The KEYSIZE with the smallest normalized edit distance is probably the key. You could proceed perhaps with the smallest 2-3 KEYSIZE values. Or take 4 KEYSIZE blocks instead of 2 and average the distances.
+
+    5) Now that you probably know the KEYSIZE: break the ciphertext into blocks of KEYSIZE length.
+
+    6) Now transpose the blocks: make a block that is the first byte of every block, and a block that is the second byte of every block, and so on.
+
+    7) Solve each block as if it was single-character XOR. You already have code to do this.
+
+    8) For each block, the single-byte XOR key that produces the best looking histogram is the repeating-key XOR key byte for that block. Put them together and you have the key.
 
   This code is going to turn out to be surprisingly useful later on. Breaking repeating-key XOR ("Vigenere") statistically is obviously an academic exercise, a "Crypto 101" thing. But more people "know how" to break it than can actually break it, and a similar technique breaks something much more important.
-  No, that's not a mistake.
 
-  We get more tech support questions for this challenge than any of the other ones. We promise, there aren't any blatant errors in this text. In particular: the "wokka wokka!!!" edit distance really is 37.
+  ## No, that's not a mistake.
+
+    We get more tech support questions for this challenge than any of the other ones. We promise, there aren't any blatant errors in this text. In particular: the "wokka wokka!!!" edit distance really is 37.
   """
 
   @doc """
